6c7c0600a593f61ada3b888705c29d05651f4bac
mmadanguit <56200606+mmadanguit@users.noreply.github.com>
2021-03-01 17:47:17 -0500

    Add files via upload

---
+++ InvisibleMapCreator/InvisibleMapCreatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Marion Madanguit on 2/26/21.
+//
+
+import SwiftUI
+
+@main
+struct invisibleMapCreatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
