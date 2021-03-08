61543c2f1a8de711a710998d6a2656b992af18f1
Marion Madanguit <mmadanguit@olin.edu>
2021-03-08 17:11:02 -0500

    Connect state machine to UI buttons

---
+++ InvisibleMapCreator/Views/SideMenuOptionView.swift
@@ -0,0 +1,20 @@
+//
+//  SideMenuOptionView.swift
+//  InvisibleMapCreator
+//
+//  Created by Marion Madanguit on 3/5/21.
+//
+
+import SwiftUI
+
+struct SideMenuOptionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SideMenuOptionView_Previews: PreviewProvider {
+    static var previews: some View {
+        SideMenuOptionView()
+    }
+}
